4b703aa0c210d9014eb23cc7ffffb81f97001394
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-17 17:52:59 +0400

    secure

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,25 +1,24 @@
 import Vue from 'vue'
+
 import firebase from 'firebase/app'
+
 import 'firebase/firebase-auth'
+
 import * as firebaseui from 'firebaseui'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyB_yC2aNHie1nPLYo-ZvaizPo26R-9eFv4',
-    authDomain: 'login-9b0d6.firebaseapp.com',
-    databaseURL: 'https://login-9b0d6.firebaseio.com',
-    projectId: 'login-9b0d6',
-    storageBucket: 'login-9b0d6.appspot.com',
-    messagingSenderId: '285518885662',
-    appId: '1:285518885662:web:01165078edc1d20f500f0f'
-  }
-
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
+}
 
 firebase.initializeApp(firebaseConfig)
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-
-
 Vue.prototype.$firebase = firebase
-
 Vue.prototype.$firebaseui = ui
